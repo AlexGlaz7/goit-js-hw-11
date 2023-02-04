2c0832764fc4c1a828e2a50f6b514cc75ed24102
Alexandr Glazunov <matveychuk1998@gmail.com>
2023-02-04 14:06:41 +0200

    Update fetch-images.js

--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 export { fetchImages };
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
-const KEY = '25766392-01b12b6ed5ab34bc2910d9c3e';
+const KEY = '33368489-8726f5d8adad5faef124f4e3c';
 
 async function fetchImages(query, page, perPage) {
   const response = await axios.get(
-    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
+    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
   );
   return response;
 }
